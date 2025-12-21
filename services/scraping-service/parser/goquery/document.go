386637dc09ec7cf6d3a8200e7e7b29b97686af7f
mahmoud-elassy <mahmoud.elassy@eduncy.com>
2025-12-21 16:07:58 +0200

    Refactor Document interface and update Scraper and Query implementations
    - Updated Document interface to return an error in the Get method for better error handling.
    - Modified Query and Scraper components to handle the new error return type.
    - Changed Scraper's results slice to store pointers to Result for improved memory management.
    - Introduced NewChromeDPFetcherRemote function for remote ChromeDP fetching.

--- services/scraping-service/parser/goquery/document.go
+++ services/scraping-service/parser/goquery/document.go
@@ -1,6 +1,8 @@
 package goquery
 
 import (
+	"fmt"
+
 	"github.com/PuerkitoBio/goquery"
 	"github.com/mahmoudelassy/distributed-scraping-system/services/scraping-service/core/contracts"
 )
@@ -9,16 +11,18 @@ type Document struct {
 	document *goquery.Document
 }
 
-func (d *Document) Get(selector string) contracts.HTMLElement {
+func (d *Document) Get(selector string) (contracts.HTMLElement, error) {
 	el := d.document.Find(selector).First()
-	return NewElement(el)
+	if el.Length() == 0 {
+		return nil, fmt.Errorf("element not found: %s", selector)
+	}
+	return NewElement(el), nil
 }
 
 func (d *Document) GetAll(selector string) []contracts.HTMLElement {
 	elements := make([]contracts.HTMLElement, 0)
 	d.document.Find(selector).Each(func(i int, s *goquery.Selection) {
-		element := NewElement(s)
-		elements = append(elements, element)
+		elements = append(elements, NewElement(s))
 	})
 	return elements
 }
